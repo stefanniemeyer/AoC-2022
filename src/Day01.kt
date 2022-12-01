c48a38fc7e6a70dec4459263cdd11c84e3501283
Stefan Niemeyer <stefan@niemeyer.name>
2022-12-01 11:25:56 +0100

    reformat day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,22 +1,35 @@
 import Resources.resourceAsText
 
 fun main() {
-    fun part1(input: String): Int {
-        val calories: List<String> = input.split("\n\n")
-        return calories.map { it.lines().filter { line -> !line.isEmpty() }.map { cals -> cals.toInt() } }.map { calI -> calI.sum() }.max()
-    }
+    fun part1(input: List<String>): Int =
+        input.map {
+            it.lines()
+                .filter { line -> !line.isEmpty() }
+                .map { calS -> calS.toInt() }
+        }.map { calI -> calI.sum() }
+            .max()
 
-    fun part2(input: String): Int {
-        val calories: List<String> = input.split("\n\n")
-        return calories.map { it.lines().filter { line -> !line.isEmpty() }.map { cals -> cals.toInt() } }.map { calI -> calI.sum() }.sorted().reversed().take(3).sum()
-    }
+
+    fun part2(input: List<String>): Int =
+        input.map {
+            it.lines()
+                .filter { line -> !line.isEmpty() }
+                .map { calS -> calS.toInt() }
+        }.map { calI -> calI.sum() }
+            .sorted()
+            .reversed()
+            .take(3)
+            .sum()
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = resourceAsText("Day01_test")
-    check(part1(testInput) == 24000)
-    check(part2(testInput) == 45000)
+    val testInput = resourceAsText("Day01_test").split("\n\n")
+    check(part1(testInput) == 24_000)
+    check(part2(testInput) == 45_000)
 
-    val input = resourceAsText("Day01")
+    val input = resourceAsText("Day01").split("\n\n")
+    check(part1(input) == 70_764)
     println(part1(input))
+    check(part2(input) == 203_905)
     println(part2(input))
 }
