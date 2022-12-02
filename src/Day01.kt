269a718ef2e32352491f41e065c902364b2ac560
Stefan Niemeyer <stefan@niemeyer.name>
2022-12-02 16:06:08 +0100

    add Day 2, refactor Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,32 +1,23 @@
 import Resources.resourceAsText
 
 fun main() {
-    fun part1(input: List<String>): Int =
-        input.map {
-            it.lines()
-                .filter { line -> !line.isEmpty() }
-                .map { calS -> calS.toInt() }
-        }.map { calI -> calI.sum() }
-            .max()
+    fun part1(input: List<Int>): Int =
+        input.first()
 
+    fun part2(input: List<Int>): Int =
+        input.take(3).sum()
 
-    fun part2(input: List<String>): Int =
-        input.map {
-            it.lines()
-                .filter { line -> !line.isEmpty() }
-                .map { calS -> calS.toInt() }
-        }.map { calI -> calI.sum() }
-            .sortedDescending()
-            .take(3)
-            .sum()
-
+    fun readInput(fileName: String): List<Int> =
+        resourceAsText(fileName).split("\n\n").map {
+            it.trim().lines().sumOf(String::toInt)
+        }.sortedDescending()
 
     // test if implementation meets criteria from the description, like:
-    val testInput = resourceAsText("Day01_test").split("\n\n")
+    val testInput = readInput("Day01_test")
     check(part1(testInput) == 24_000)
     check(part2(testInput) == 45_000)
 
-    val input = resourceAsText("Day01").split("\n\n")
+    val input = readInput("Day01")
     check(part1(input) == 70_764)
     println(part1(input))
     check(part2(input) == 203_905)
---
+++ src/Day02.kt
@@ -0,0 +1,95 @@
+import Resources.resourceAsList
+
+fun main() {
+    fun part1(input: List<Pair<String, String>>): Int =
+        input.fold(0) { acc, op ->
+            acc + score(op.first, op.second)
+        }
+
+    fun part2(input: List<Pair<String, String>>): Int =
+        input.fold(0) { acc, op ->
+            acc + score(op.first, op.second)
+        }
+
+    var name = Throwable().stackTrace.first { it.className.contains("Day") }.className.split(".")[0]
+    name = name.removeSuffix("Kt")
+    // test if implementation meets criteria from the description, like:
+    val testInput = resourceAsList(fileName = "${name}_test").map { it.toPair() }
+    val puzzleInput = resourceAsList(fileName = name).map { it.toPair() }
+
+    check(part1(testInput) == 15)
+    check(part1(puzzleInput) == 12156)
+    println(part1(puzzleInput))
+
+    val testInput2 = resourceAsList(fileName = "${name}_test").map { it.toPairByResult() }
+    val puzzleInput2 = resourceAsList(fileName = name).map { it.toPairByResult() }
+
+    check(part2(testInput2) == 12)
+    check(part2(puzzleInput2) == 10835)
+
+    println(part2(puzzleInput2))
+}
+
+fun String.toPair(): Pair<String, String> =
+    this.split(" ").let { it[0].toElfShape() to it[1].toElfShape() }
+
+fun String.toPairByResult(): Pair<String, String> =
+    this.split(" ").let { it[0].toElfShape() to it[1].toShapeByResult(it[0].toElfShape()) }
+
+fun String.toElfShape(): String = when (this) {
+    "X" -> "R"
+    "Y" -> "P"
+    "Z" -> "S"
+    "A" -> "R"
+    "B" -> "P"
+    "C" -> "S"
+    else -> throw IllegalArgumentException("Unknown shape: $this")
+}
+
+fun String.toShapeByResult(elf: String): String =
+    when (this) {
+        "Y" -> elf  // draw
+        "X" -> when (elf) {    // lose
+            "R" -> "S"
+            "P" -> "R"
+            "S" -> "P"
+            else -> throw IllegalArgumentException("Unknown shape: $elf")
+        }
+
+        "Z" -> when (elf) {    // win
+            "R" -> "P"
+            "P" -> "S"
+            "S" -> "R"
+            else -> throw IllegalArgumentException("Unknown shape: $elf")
+        }
+
+        else -> throw IllegalArgumentException("Unknown result: $this")
+    }
+
+fun shapeScore(shape: String) =
+    when (shape) {
+        "R" -> 1
+        "P" -> 2
+        "S" -> 3
+        else -> 0
+    }
+
+fun score(elf: String, player: String): Int {
+    val baseScore = shapeScore(player)
+    if (elf == player) {
+        return 3 + baseScore
+    }
+    when (player) {
+        "R" -> if (elf == "S") return 6 + baseScore
+        "P" -> if (elf == "R") return 6 + baseScore
+        "S" -> if (elf == "P") return 6 + baseScore
+    }
+    return 0 + baseScore
+}
+// A, X Rock       1
+// B, Y Paper      2
+// C, Z Scissors   3
+
+// X lose
+// Y draw
+// Z win
