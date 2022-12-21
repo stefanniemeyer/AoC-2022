a4e4ee8927de51471f075bea559423382bfed356
Stefan Niemeyer <stefan@niemeyer.de>
2022-12-21 13:29:52 +0100

    add day 21 part 2

--- src/de/niemeyer/aoc2022/Day21.kt
+++ src/de/niemeyer/aoc2022/Day21.kt
@@ -8,19 +8,77 @@ package de.niemeyer.aoc2022
 import de.niemeyer.aoc2022.Resources.resourceAsList
 
 fun main() {
+    val root = "root"
+    val human = "humn"
+
     abstract class MonkeyMath(val name: String) {
         abstract fun calc(): Long
+        abstract fun restoreResult(result: Long): Long
+        abstract fun containsName(searchName: String): Boolean
+        fun containsHuman() = containsName(human)
+        fun isHuman() = name == human
     }
 
     var monkeys: Map<String, MonkeyMath> = mapOf()
 
     class MonkeyYell(name: String, val number: Long) : MonkeyMath(name) {
         override fun calc(): Long = number
+        override fun restoreResult(result: Long): Long =
+            error("don't call restoreResult on MonkeyYell")
+
+        override fun containsName(searchName: String): Boolean =
+            name == searchName
     }
 
-    class MonkeyCalc(name: String, val left: String, val right: String, val func: (a: Long, b: Long) -> Long) :
+    class MonkeyCalc(
+        name: String,
+        val left: String,
+        val right: String,
+        val func: (a: Long, b: Long) -> Long,
+        val inverseFunc: (a: Long, b: Long) -> Long,
+        val commutation: Boolean
+    ) :
         MonkeyMath(name) {
+        val leftMonkey by lazy { monkeys.getValue(left) }
+        val rightMonkey by lazy { monkeys.getValue(right) }
         override fun calc(): Long = func(monkeys.getValue(left).calc(), monkeys.getValue(right).calc())
+        override fun restoreResult(result: Long): Long {
+            return if (leftMonkey.isHuman()) {
+                val rightResult = rightMonkey.calc()
+                val newResult = inverseFunc(result, rightResult)
+                newResult
+            } else if (rightMonkey.isHuman()) {
+                val leftResult = leftMonkey.calc()
+                var newResult: Long
+                if (commutation) {
+                    newResult = inverseFunc(result, leftResult)
+                } else {
+                    newResult = func(leftResult, result)
+                }
+                newResult
+            } else if (leftMonkey.containsHuman()) {
+                val rightResult = rightMonkey.calc()
+                val newResult = inverseFunc(result, rightResult)
+                val leftResult = leftMonkey.restoreResult(newResult)
+                leftResult
+            } else if (rightMonkey.containsHuman()) {
+                val leftResult = leftMonkey.calc()
+                val newResult: Long
+                if (commutation) {
+                    newResult = inverseFunc(result, leftResult)
+                } else {
+                    newResult = func(leftResult, result)
+                }
+                val rightResult = rightMonkey.restoreResult(newResult)
+                rightResult
+            } else {
+                result
+            }
+        }
+
+        override fun containsName(searchName: String): Boolean =
+            name == searchName || monkeys.getValue(left).containsName(searchName) || monkeys.getValue(right)
+                .containsName(searchName)
     }
 
     fun createMonkey(input: String): MonkeyMath {
@@ -33,10 +91,10 @@ fun main() {
         val right = params.last()
 
         when (params[1]) {
-            "+" -> return MonkeyCalc(monkeyName, left, right, Long::plus)
-            "-" -> return MonkeyCalc(monkeyName, left, right, Long::minus)
-            "*" -> return MonkeyCalc(monkeyName, left, right, Long::times)
-            "/" -> return MonkeyCalc(monkeyName, left, right, Long::div)
+            "+" -> return MonkeyCalc(monkeyName, left, right, Long::plus, Long::minus, commutation = true)
+            "-" -> return MonkeyCalc(monkeyName, left, right, Long::minus, Long::plus, commutation = false)
+            "*" -> return MonkeyCalc(monkeyName, left, right, Long::times, Long::div, commutation = true)
+            "/" -> return MonkeyCalc(monkeyName, left, right, Long::div, Long::times, commutation = false)
             else -> error("unknown operator: ${params[1]}")
         }
     }
@@ -46,11 +104,33 @@ fun main() {
 
     fun part1(input: List<String>): Long {
         monkeys = parse(input)
-        return monkeys.getValue("root").calc()
+        return monkeys.getValue(root).calc()
     }
 
-    fun part2(monkeys: List<String>): Long =
-        TODO()
+    fun part2(input: List<String>): Long {
+        monkeys = parse(input)
+        val rootMonkey = monkeys.getValue(root)
+        if (rootMonkey is MonkeyCalc) {
+            val leftMonkey = rootMonkey.leftMonkey
+            val rightMonkey = rootMonkey.rightMonkey
+            val noHumanResult: Long
+            val humanResult: Long
+            val humanLeft = leftMonkey.containsHuman()
+            val humanRight = rightMonkey.containsHuman()
+            check(humanLeft || humanRight) { "'$human' not found" }
+            check(!(humanLeft && humanRight)) { "two '$human' found" }
+
+            if (humanLeft) {    // calculate the tree w/o the human
+                noHumanResult = rightMonkey.calc()
+                humanResult = leftMonkey.restoreResult(noHumanResult)
+            } else {
+                noHumanResult = leftMonkey.calc()
+                humanResult = rightMonkey.restoreResult(noHumanResult)
+            }
+            return humanResult
+        }
+        error("root monkey is not a MonkeyCalc")
+    }
 
     val name = getClassName()
     val testInput = resourceAsList(fileName = "${name}_test")
@@ -61,8 +141,8 @@ fun main() {
     println(puzzleResultPart1)
     check(puzzleResultPart1 == 84_244_467_642_604L)
 
-//    check(part2(testInput) == 0)
-//    val puzzleResultPart2 = part2(puzzleInput)
-//    println(puzzleResultPart2)
-//    check(puzzleResultPart2 == 0)
+    check(part2(testInput) == 301L)
+    val puzzleResultPart2 = part2(puzzleInput)
+    println(puzzleResultPart2)
+    check(puzzleResultPart2 == 3_759_569_926_192L)
 }
